651cfe2a81d09d87ba95321054394389f328c6de
Robert George <robert.george@qwikcilver.com>
2016-01-21 11:27:47 +0530

    Basic styling for sell card form

--- resources/views/sell/add.blade.php
+++ resources/views/sell/add.blade.php
@@ -3,18 +3,45 @@
 @section('content')
 <div class="containter">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel-body">
-				<form method="post">
-					<label>Card Number</label>
-					<input type="text" name="card-number">
-					<br>
-					<label>Pin</label>
-					<input type="password" name="pin" size="4">
-					<br/>
-					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<input type="submit" value="Sell">
-				</form>
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Sell your gift card</div>
+				<div class="panel-body">
+					<form class="form-horizontal" role="form" method="POST">
+					{!! csrf_field() !!}
+					<div class="form-group">
+						<label class="col-md-4 control-label">
+							Card Number
+						</label>
+						<div class="col-md-6">
+							<input type="text" class="form-control" name="card-number">
+							@if($errors->has('card-number'))
+								<span class="helper-block">
+									<strong>
+									</strong>
+								</span>
+							@endif
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-4 control-label">
+							Pin
+						</label>
+						<div class="col-md-6">
+							<input type="password" name="pin" class="form-control" size="4">
+						</div>
+					</div>
+					<div class="form-group">
+					  <div class="col-md-6 col-md-offset-4">
+					  	<button type="submit" class="btn btn-primary">
+						  <i class="fa fa-btn fa-sign-in"></i>Validate Card
+						</button>
+
+					  </div>
+					</div>
+
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
