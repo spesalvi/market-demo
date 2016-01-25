9541c4c0ceb5cdd480910cfb5780e192d2db279a
Robert George <robert.george@qwikcilver.com>
2016-01-25 08:40:57 +0530

    Percentage off calculation for offers

--- resources/views/listing/all.blade.php
+++ resources/views/listing/all.blade.php
@@ -21,7 +21,7 @@
 			<tr>
 			<td><img width="68" height="68" src="http://devcdn.giftbig.com/dev3/brands_logo/large/3_logo.png"></img></td>
 			<td>{{ $giftcard['balance'] }}</td>
-			<td>10%</td>
+			<td>{{ number_format(round(($giftcard['balance'] - $giftcard['offer_price'])*100)/$giftcard['balance'], 2)}} %</td>
 			<td>{{ $giftcard['offer_price'] }}</td>
 			<td> {{ $giftcard['expiry_date'] }} </td>
 			<td>
